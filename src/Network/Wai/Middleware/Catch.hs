eb6648d6a98e15f0a0ad7c7eaa3a097745a0d3b1
Alexander Dorofeev <aka.spin@gmail.com>
2012-01-24 05:13:31 +0500

    Switch to conduit
    Signed-off-by: Alexander Dorofeev <aka.spin@gmail.com>

--- src/Network/Wai/Middleware/Catch.hs
+++ src/Network/Wai/Middleware/Catch.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- | Exception handling for 'Wai' and 'Warp'.
 --
@@ -50,27 +52,15 @@
 --   To ensure catch all errors, you need to consume all data /before/ feeding 
 --   the builder. 
 
-module Network.Wai.Middleware.Catch (
-    protect,
-    protect'
-) where
+module Network.Wai.Middleware.Catch where
 
 import Prelude hiding (catch)
-
--- control
-import Control.Monad.Trans.Control (MonadBaseControl)
+import Control.Exception.Lifted (catch)
 import Control.Exception (SomeException)
 import qualified Control.Exception.Lifted as E
-
--- enumerator
-import Data.Enumerator (Iteratee (..), Step (..))
-
--- bytestrings
 import qualified Data.ByteString.Lazy.Char8 as BL (pack)
-
--- networking
 import Network.HTTP.Types (status500)
-import Network.Wai
+import Network.Wai (Application, Middleware, responseLBS)
 
 -- | Handles exceptions in responses. If exception isn't handled - it will be 
 --   rethrown further. To ensure handle all errors use 'protect''.
@@ -80,7 +70,7 @@ protect :: (E.Exception e) =>
 protect handler app req = app req `catch` (`handler`req)
 
 -- | Strict version of 'protect'. Handles all exceptions. If exception not 
---   handled, this function return /500 Internal Server Error/ with empty 
+--   handled, this function return @500 - Internal Server Error@ with empty 
 --   headers and body what contains 'show' of error.
 protect' :: (E.Exception e) =>
        (e -> Application)   -- ^ Handler
@@ -91,19 +81,5 @@ protect' handler app req =
     defHandler :: SomeException -> Application
     defHandler e _ = return $ responseLBS status500 [] $ BL.pack $ show e
 
-------------------------------------------------------------------------------
--- Helpers
-------------------------------------------------------------------------------
 
--- | Catch exceptions inside enumerator
-catch :: (E.Exception e, MonadBaseControl IO m)
-      => Iteratee a m b
-      -> (e -> Iteratee a m b)
-      -> Iteratee a m b
-catch (Iteratee mstep) f = Iteratee $ do
-    step <- mstep `E.catch` (runIteratee . f)
-    return $ case step of
-        Continue k -> Continue $ \s -> catch (k s) f
-        Yield b s -> Yield b s
-        Error e -> Error e
 
